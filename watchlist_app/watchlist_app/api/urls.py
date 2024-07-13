8c198dc1823d6107e596c82ce3b6211212c03c53
danofoundation <dan.dev947366@gmail.com>
2024-07-13 21:59:31 +0300

    Add APIView

--- watchlist_app/watchlist_app/api/urls.py
+++ watchlist_app/watchlist_app/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 
-from watchlist_app.api.views import movie_details, movie_list
+# from watchlist_app.api.views import movie_details, movie_list
+from watchlist_app.api.views import MovieDetailAV, MovieListAV
 
 urlpatterns = [
-    path("list/", movie_list, name="movie-list"),
-    path("<int:pk>", movie_details, name="movie-detail"),
+    # path("list/", movie_list, name="movie-list"),
+    # path("<int:pk>", movie_details, name="movie-detail"),
+    path("list/", MovieListAV.as_view(), name="movie-list"),
+    path("<int:pk>", MovieDetailAV.as_view(), name="movie-detail"),
 ]
