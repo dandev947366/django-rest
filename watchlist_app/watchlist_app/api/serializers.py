a1492031249b231aee9f8ad5c5cea9d78f367167
danofoundation <dan.dev947366@gmail.com>
2024-07-14 18:17:26 +0300

    Add ModelSerializer

--- watchlist_app/watchlist_app/api/serializers.py
+++ watchlist_app/watchlist_app/api/serializers.py
@@ -1,24 +1,36 @@
 from rest_framework import serializers
 from watchmate.models import Movie
 
+# class MovieSerializer(serializers.Serializer):
+# id = serializers.IntegerField(read_only=True)
+# name = serializers.CharField()
+# description = serializers.CharField()
+# active = serializers.BooleanField()
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+# def create(self, validated_data):
+#     return Movie.objects.create(**validated_data)
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
 
-    def update(
-        self, instance, validated_data
-    ):  # instance=old_value, validated_date=new_value
-        instance.name = validated_data.get("name", instance.name)
-        instance.description = validated_data.get("description", instance.description)
-        instance.active = validated_data.get("active", instance.active)
-        instance.save()
-        return instance
+# def update(
+#     self, instance, validated_data
+# ):  # instance=old_value, validated_date=new_value
+#     instance.name = validated_data.get("name", instance.name)
+#     instance.description = validated_data.get("description", instance.description)
+#     instance.active = validated_data.get("active", instance.active)
+#     instance.save()
+#     return instance
+
+
+# NOTE - Model serializers
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+    def get_len_name(self, object):
+        return len(object.name)
 
     def validate_name(self, value):
         if len(value) < 2:
